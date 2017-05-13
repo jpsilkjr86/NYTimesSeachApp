5f8923afa2ead71436ef55fa13b63cc1520db213
John Silkey <John Silkey>
2017-05-13 13:42:09 -0700

    search submit event listener ready

--- appJava.js
+++ appJava.js
@@ -1,34 +1,40 @@
 
-var apiKey = '29733341ec5d48809c6e4c6d645d281d';
-var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey;
+$('#Search').on('click', function(e){
+	e.preventDefault();
 
-var searchQ = 'q=dogs';
-var beginDate = 'begin_date=20160901';
-var page = '0';
-var sampleQuery = queryURL + '&' + searchQ + '&' + page + '&' + beginDate;
+	var apiKey = '29733341ec5d48809c6e4c6d645d281d';
+	var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey;
 
-
-
-
-$.ajax({
-	url: queryURL,
-	method: 'GET'
-}).done(function(response){
-	var docs = response.response.docs
-	console.log(docs);
+	var searchQ = 'q=dogs';
+	var beginDate = 'begin_date=20160901';
+	var page = '0';
+	var sampleQuery = queryURL + '&' + searchQ + '&' + page + '&' + beginDate;
 
 
-	for (i = 0; i < docs.length; i++) {
-		var newPanel = $('<div class="panel panel-default">');
 
-		var panelHead = $('<div class="panel-heading">');
-		panelHead.text('Insert title here');
 
-		var panelContent = $('<div class="panel-body">');
-		panelContent.text('Some content');
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).done(function(response){
+		var docs = response.response.docs
+		console.log(docs);
 
-		newPanel.append(panelHead, panelContent);
-		$('#Articles').append(newPanel);
-	}
 
+		for (i = 0; i < docs.length; i++) {
+			var newPanel = $('<div class="panel panel-default">');
+
+			var panelHead = $('<div class="panel-heading">');
+			panelHead.text('Insert title here');
+
+			var panelContent = $('<div class="panel-body">');
+			panelContent.text('Some content');
+
+			newPanel.append(panelHead, panelContent);
+			$('#Articles').append(newPanel);
+		}
+
+	});
 });
+
+	
