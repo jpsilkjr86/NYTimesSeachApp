babed3b0e0737017809ac1834b0605d090db547f
John Silkey <John Silkey>
2017-05-13 13:14:02 -0700

    cleaner sample

--- appJava.js
+++ appJava.js
@@ -1,29 +1,21 @@
-<<<<<<< HEAD
+
 var apiKey = '29733341ec5d48809c6e4c6d645d281d';
 var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey;
 
-var searchQ = 
-var sampleQuery = 'q=dogs&page=0&begin_date=20160901';
+var searchQ = 'q=dogs';
+var beginDate = 'begin_date=20160901';
+var page = '0';
+var sampleQuery = queryURL + '&' + searchQ + '&' + page + '&' + beginDate;
 
 
-queryURL += sampleQuery;
-=======
-// api key = 29733341ec5d48809c6e4c6d645d281d
-var queryURL = 'https://api.nytimes/com/svc/search/v2/articlesearch.json';
->>>>>>> e762175dadea90690071a41bf63cb9e7ede6ad9d
-//search by keyword = ?q=
-//set return limit = page = 
-//set start year = begin_date =
+
 
 $.ajax({
 	url: queryURL,
 	method: 'GET'
 }).done(function(response){
-<<<<<<< HEAD
 	var docs = response.response.docs
 	console.log(docs);
-=======
 
->>>>>>> e762175dadea90690071a41bf63cb9e7ede6ad9d
 
 });
