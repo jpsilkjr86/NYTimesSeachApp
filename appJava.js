f14b9303b30eae2ccc1a5fc3b2f74713e1ace349
John Silkey <John Silkey>
2017-05-13 13:07:38 -0700

    john first

--- appJava.js
+++ appJava.js
@@ -1,7 +1,9 @@
 var apiKey = '29733341ec5d48809c6e4c6d645d281d';
-var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
+var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey;
 
-var sampleQuery = 'q=dogs&api-key=' + apiKey + '&page=0&begin_date=20160901';
+var searchQ = 
+var sampleQuery = 'q=dogs&page=0&begin_date=20160901';
+
 
 queryURL += sampleQuery;
 //search by keyword = ?q=
